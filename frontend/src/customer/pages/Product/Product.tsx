0ea7a421ba4607a0771ee1b0efc5c608d6224844
Himanshu Gupta <himanshugupta2068ba@gmail.com>
2026-01-15 22:26:14 +0530

    fix: correct product variable name and update ProductCard to display seller information

--- frontend/src/customer/pages/Product/Product.tsx
+++ frontend/src/customer/pages/Product/Product.tsx
@@ -4,13 +4,18 @@ import { useState } from "react";
 import { ProductCard } from "./ProductCard";
 
 
-const products=
+const product=
     {
         images:[
       "https://images.unsplash.com/photo-1580854898508-4761a9c769a9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d29tZW4lMjBzYXJlZXxlbnwwfHwwfHx8MA%3D%3D",
       "https://images.unsplash.com/photo-1756483482418-3f3e4c13f9b0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBzYXJlZXxlbnwwfHwwfHx8MA%3D%3D",
-      "https://images.unsplash.com/photo-1677002419193-9a74069587af?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWVuJTIwc2FyZWV8ZW58MHx8MHx8fDA%3D",
-    ]
+      "https://images.unsplash.com/photo-1677002419193-9a74069587af?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWVuJTIwc2FyZWV8ZW58MHx8MHx8fDA%3D"
+    ],
+    seller:{
+        bussinessDetails:{
+            bussinessName:"Saree Shop"
+        }
+    }
 }
 
 
@@ -64,9 +69,9 @@ export const Products=()=>{
                          <Divider/>
 
                          <div className="grid sm:grid-cols-2 md:grid-cols3 lg:grid-cols-4 gap-y-5 px-5 justify-center">
-                            {[1,1,1,1,1,1,1,1,1,1,1,1].map((item,index)=>
-                            <div key={index} className="flex justify-center">
-                                <ProductCard item={products} />
+                            {[1,1,1,1,1,1].map((item,index)=>
+                            <div key={index*3} className="flex justify-center">
+                                <ProductCard item={product} />
                             </div>
 
                             )}
--- frontend/src/customer/pages/Product/ProductCard.tsx
+++ frontend/src/customer/pages/Product/ProductCard.tsx
@@ -3,65 +3,47 @@ import "./ProductCard.css"
 
 export const ProductCard = ({item}:any) => {
 
-    const [ishovered,setIshovered]=useState(false);
     const [currentImage,setCurrentImage]=useState(0);
+    
+    const [ishovered,setIshovered]=useState(false);
 
     useEffect(()=>{
         let interval:any;
         if(ishovered){
-            interval=setInterval(()=>{
-                setCurrentImage((prevImage)=>{
-                    if(prevImage===item.images.length-1){
-                        return 0;
-                    }else{
-                        return prevImage+1;
-                    }
-                })
-            },1000)
-        }else{
-            setCurrentImage(0);
-            clearInterval(interval);
+           interval=setInterval(()=>setCurrentImage((prev)=>(prev+1)%item.images.length),1000);
 
         }
-    },[ishovered]);
-
-// useEffect(() => {
-//   if (!ishovered) {
-//     setCurrentImage(0);
-//     return;
-//   }
-
-//   const interval = setInterval(() => {
-//     setCurrentImage((prev) =>
-//       prev === item.images.length - 1 ? 0 : prev + 1
-//     );
-//   }, 1000);
-
-//   return () => clearInterval(interval);
-// }, [ishovered, item.images.length]);
-
+        else if(interval){
+            clearInterval(interval);
+        }
+        return ()=>clearInterval(interval);
+    },[ishovered,item.images.length]);
 
     return (
         <div className="group px-4 relative">
            <div className="relative w-[250px] sm:w-full h-[350px] overflow-hidden" onMouseEnter={()=>setIshovered(true)} onMouseLeave={()=>setIshovered(false)}>
 
-            {/* {item.images.map((image:string,index:number)=>
-            <img src={image} key={index} className="card-media object-top" style={{transform:`translateX(${(index-currentImage)*100}%)`,}} />
-            )} */}
-
- {item.images.map((image: string, index: number) => (
-  <img
-    key={index}
-    src={image}
-    className="card-media object-top"
-    style={{
-      transform: `translateX(${(index - currentImage) * 100}%)`,
-    }}
-  />
-))}
-
+            {item.images.map((image:string,index:number)=>
+            <img src={image} key={index} className="card-media object-top" style={{transform:`translateX(${(index-currentImage)*100}%)`}} />
+            )}
 
            </div>
+      
+           <div className="details pt-3 space-y-1 group-hover-effect rounded-md">
+            <div className="name space-y">
+                <h1 className="font-semibold text-lg">
+                    {item.seller?.bussinessDetails?.bussinessName}
+                </h1>
+                <p>Marron Floral Patteren Saree</p>
+            </div>
+            <div className="price flex items-center gap-3">
+                <span className="font-semibold text-teal-800">  ₹2999 </span>
+                <span className="line-through text-gray-500 text-sm"> ₹3999 </span>
+                <span className="text-green-600 text-sm"> 25% off </span>
+                
+            </div>
+           </div>
+
         </div>
     )
 }
