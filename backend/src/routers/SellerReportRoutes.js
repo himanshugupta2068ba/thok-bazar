c55c301811fd47996b3a23a7c840388d0db4aafc
Himanshu Gupta <himanshugupta2068ba@gmail.com>
2026-03-28 00:31:59 +0530

    fix: correct casing in sellerReportController import

--- backend/src/routers/SellerReportRoutes.js
+++ backend/src/routers/SellerReportRoutes.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const sellerMiddleware=require('../middlewares/sellerAuthMiddleware');
-const sellerReportController=require('../controllers/SellerReportController');
+const sellerReportController=require('../controllers/sellerReportController');
 
 router.get('/',sellerMiddleware,sellerReportController.getSellerReport);
 
